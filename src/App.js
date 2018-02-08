d5fe90b5f88395f7b44f4021c923b64df3abfc2d
Conner Gillette <cg1100@uw.edu>
2018-02-07 17:35:39 -0800

    Add visual improvements to windows

--- src/App.js
+++ src/App.js
@@ -10,9 +10,24 @@ class App extends Component {
           </div>
           <div className="container">
               <div className="row">
-                  <div className="content-window preview col-md-6">
+                  <div className="content-window col-md-6">
+                      <div className="content-container preview">
+                      </div>
                   </div>
-                  <div className="content-window form col-md-6">
+                  <div className="content-window col-md-6">
+                      <div className="content-container form">
+                          <form>
+                              <div className="form-group">
+                                <label for="exampleInputEmail1">Email address</label>
+                                <input type="email" className="form-control form-control-lg" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                              </div>
+                              <div className="form-group">
+                                <label for="exampleInputPassword1">Password</label>
+                                <input type="password" className="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password" />
+                          </div>
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                      </form>
+                    </div>
                   </div>
               </div>
           </div>
