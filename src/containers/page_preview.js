d3be0e518d8007cdcb954e4c737c9dfc8ef6c5e4
Conner Gillette <cg1100@uw.edu>
2018-04-23 09:57:13 -0700

    Add comment of basic plan for page preview

--- src/containers/page_preview.js
+++ src/containers/page_preview.js
@@ -17,3 +17,14 @@ class PagePreview extends Component {
 }
 
 export default PagePreview;
+
+/**
+ * Apply this idea in reverse:
+ * Make a base template with ONLY React elements (no basic HTML)
+ * Re-make the scripts necessary for editing
+ * Track changes with sidebar
+ * 
+ * THEN allow for HTML importing
+ * 
+ * Boom.
+ */
